1adebd498a70d763838d7eaa72d4deb2dd9e07c6
SdCode000 <shamnadsdd>
2024-08-21 20:06:16 +0530

    AST

---
+++ assignment.js
@@ -0,0 +1,43 @@
+ // assignment
+    // 1
+    // const books=[
+    //     {
+    //         title :'the Great gatsby',
+    //         author :'f.scott',
+
+    //     },
+    //     {
+    //         title :'kill',
+    //         author:'harper lee',
+    //         publication:'1997'
+
+    //     },
+    //     {
+    //         title :'the lord of the rings',
+    //         author :'j.r.r.tolkien',
+    //     },
+
+    // ]
+
+    // const[book1,book2,book3] =books;
+
+    // // 2
+    // let {publication:publication}=book2
+    // console.log(`book :2 is the ${book2.title}author is ${book2.author} pb is ${publication}`)
+
+    // // 3
+    // const book4 =[
+    //     {
+    //         title:'harry potter',
+    //         author:'j.k.rowling',
+
+    //     }
+    // ]
+    // console.log(book4);
+
+    // 4
+    // const bookss =[...books,...book4]
+    // console.log(bookss);
+
+    // const copiedBooks =[...books]
+    // console.log(copiedBooks);
